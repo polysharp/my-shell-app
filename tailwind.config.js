5c3a4db3168c74728ef0af10c21806d7b681fd9b
Henri Fardeau <henri.fardeau@outlook.fr>
2020-09-18 13:41:11 +0200

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   purge: false,
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   theme: {
     container: {
       center: true,
     },
     extend: {
       colors: {
-        primary: '#FF5858',
+        primary: '#000000',
       },
     },
   },
